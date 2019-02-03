0693584f8aa3a44dc41ff7d370bd49f3eef370cb
Elitsa Venchovs <elica_vladi@abv.bg>
2019-02-04 00:05:48 +0200

    Оправени проблеми с четенето и разминаването с линийте
    Четенето вече става до прочитане на терминираш символ. Преместването на моторите е стъпко по стъпка едновремено.

--- Arduino/ploter_draw/ploter_draw.ino
+++ Arduino/ploter_draw/ploter_draw.ino
@@ -28,7 +28,7 @@ const int MAX_X = 200 * 11 + 50;
 const int MAX_Y = 200 * 9;
 
 //Горна и долна позиция на сервото
-const int PEN_UP_POS = 150;
+const int PEN_UP_POS = 160;
 const int PEN_DOWN_POS = 135;
 
 int currentX = 0, currentY = 0; //текуща позиция по X и Y
@@ -88,22 +88,26 @@ int readData() {
   int res;
   int c;
   int v = -1;
+  int isDelimRead = false;
 
-  while (Serial.available()) {
-    c = Serial.read();
+  while (!isDelimRead) {
+    while (Serial.available()) {
+      c = Serial.read();
 
-    // handle digits
-    if ((c >= '0') && (c <= '9')) {
-      if (v == -1) {
-        v = 0;
+      // handle digits
+      if ((c >= '0') && (c <= '9')) {
+        if (v == -1) {
+          v = 0;
+        }
+        v = 10 * v + c - '0';
       }
-      v = 10 * v + c - '0';
-    }
-    // handle delimiter
-    else if (c == 'e' && v != -1) {
-      res = v;
-      v = -1;
-      break;
+      // handle delimiter
+      else if (c == 'e' && v != -1) {
+        res = v;
+        v = -1;
+        isDelimRead = true;
+        break;
+      }
     }
   }
   Serial.println(res);
@@ -138,101 +142,22 @@ void movePen() {
 //Преместване на позията по зададения код на Фрийман
 void move(int freemanCode) {
   if (freemanCode == DIR_UP) {
-    moveStepX(LOW);
+    moveStep(LOW, -1);
   } else if (freemanCode == DIR_UP_RIGHT) {
-    moveStepX(LOW);
-    moveStepY(HIGH);
+    moveStep(LOW, HIGH);
   } else if (freemanCode == DIR_RIGHT) {
-    moveStepY(HIGH);
+    moveStep(-1, HIGH);
   } else if (freemanCode == DIR_DOWN_RIGHT) {
-    moveStepX(HIGH);
-    moveStepY(HIGH);
+    moveStep(HIGH, HIGH);
   } else if (freemanCode == DIR_DOWN) {
-    moveStepX(HIGH);
+    moveStep(HIGH, -1);
   } else if (freemanCode == DIR_DOWN_LEFT) {
-    moveStepX(HIGH);
-    moveStepY(LOW);
+    moveStep(HIGH, LOW);
   } else if (freemanCode == DIR_LEFT) {
-    moveStepY(LOW);
+    moveStep(-1, LOW);
   } else if (freemanCode == DIR_UP_LEFT) {
-    moveStepX(LOW);
-    moveStepY(LOW);
+    moveStep(LOW, LOW);
   }
-}
-
-//Правене на стъпка по X и по Y.
-//В отделни фуции са, защото може да се окаже, че една стъпка ще бъде повече от 1 оборот
-void moveStepX(int dir) {
-  int lenght = 0;
-  if (!isPrevPenUp) {
-    if (!isPenUp) {
-      lenght = 40;
-    } else {
-      lenght = 60;
-    }
-  } else {
-    if (!isPenUp) {
-      lenght = -1;
-    } else {
-      lenght = 30;
-    }
-  }
-
-  digitalWrite(dirPinX, dir);//задавае на посоката
-  for (int x = 0; x < lenght; x++) {
-    if (currentX >= 0 && currentX <= MAX_X) {
-      digitalWrite(stepPinX, HIGH);
-      delayMicroseconds(500);
-      digitalWrite(stepPinX, LOW);
-      delay(20);
-      if (dir == HIGH) {
-        currentX++;
-      } else {
-        currentX--;
-      }
-    } else {
-      Serial.println("X --> -1");
-      break;
-    }
-  }
-  isPrevPenUp = isPenUp;
-  delay(20);
-}
-void moveStepY(int dir) {
-  int lenght = 0;
-  if (!isPrevPenUp) {
-    if (!isPenUp) {
-      lenght = 40;
-    } else {
-      lenght = 60;
-    }
-  } else {
-    if (!isPenUp) {
-      lenght = -1;
-    } else {
-      lenght = 30;
-    }
-  }
-
-  digitalWrite(dirPinY, dir);//задавае на посоката
-  for (int x = 0; x < lenght; x++) {
-    if (currentY >= 0 && currentY <= MAX_X) {
-      digitalWrite(stepPinY, HIGH);
-      delayMicroseconds(500);
-      digitalWrite(stepPinY, LOW);
-      delay(20);
-      if (dir == HIGH) {
-        currentY++;
-      } else {
-        currentY--;
-      }
-    } else {
-      Serial.println("Y --> -1");
-      break;
-    }
-  }
-  isPrevPenUp = isPenUp;
-  delay(20);
 }
 
 void moveStep(int dirX, int dirY) {
@@ -241,20 +166,22 @@ void moveStep(int dirX, int dirY) {
     if (!isPenUp) {
       lenght = 40;
     } else {
-      lenght = 60;
+      lenght = 40;
     }
   } else {
     if (!isPenUp) {
       lenght = -1;
     } else {
-      lenght = 30;
+      lenght = 40;
     }
   }
+
+  boolean isXOk = true, isYOk = true;
 
   digitalWrite(dirPinX, dirX);//задавае на посоката
   digitalWrite(dirPinY, dirY);//задавае на посоката
   for (int x = 0; x < lenght; x++) {
-    if (currentX >= 0 && currentX <= MAX_X && dirX != -1) {
+    if ((currentX > 0 && dirX == LOW) || (currentX < MAX_X && dirX == HIGH)) {
       digitalWrite(stepPinX, HIGH);
       delayMicroseconds(500);
       digitalWrite(stepPinX, LOW);
@@ -264,23 +191,25 @@ void moveStep(int dirX, int dirY) {
       } else {
         currentX--;
       }
-    } else if(dirX != -1) {
+    } else if (dirX != -1 && isXOk) {
       Serial.println("X --> -1");
+      isXOk = false;
     }
-    
-    if (currentY >= 0 && currentY <= MAX_X && dirY != -1) {
+
+    if ((currentY > 0 && dirY == LOW) || (currentY < MAX_Y && dirY == HIGH)) {
       digitalWrite(stepPinY, HIGH);
       delayMicroseconds(500);
       digitalWrite(stepPinY, LOW);
       delay(20);
-      if (dirY== HIGH) {
+      if (dirY == HIGH) {
         currentY++;
       } else {
         currentY--;
       }
-    } else if(dirY != -1) {
+    } else if (dirY != -1 && isYOk) {
       Serial.println("Y --> -1");
-    }    
+      isYOk = false;
+    }
   }
   isPrevPenUp = isPenUp;
   delay(20);
